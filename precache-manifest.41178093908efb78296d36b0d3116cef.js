98c9616a9efaf7a433ff374a1c492296e705c4ba
José Pérez Negro <peresnegro@hotmail.com>
2020-06-03 03:39:40 +0100

    Updates

---
+++ precache-manifest.41178093908efb78296d36b0d3116cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015bc097ee7a6837b53e2e3bb7d05058",
+    "url": "/portugalasmanchetes/index.html"
+  },
+  {
+    "revision": "3e3c4d6ac18ae5c03e66",
+    "url": "/portugalasmanchetes/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "46536115366bac1d2d6c",
+    "url": "/portugalasmanchetes/static/css/main.86aed4bd.chunk.css"
+  },
+  {
+    "revision": "3e3c4d6ac18ae5c03e66",
+    "url": "/portugalasmanchetes/static/js/2.6a7e03ce.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portugalasmanchetes/static/js/2.6a7e03ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46536115366bac1d2d6c",
+    "url": "/portugalasmanchetes/static/js/main.4829b8a5.chunk.js"
+  },
+  {
+    "revision": "7f5aadb5dd327b29e5a6",
+    "url": "/portugalasmanchetes/static/js/runtime-main.aff7e2d8.js"
+  }
+]);
