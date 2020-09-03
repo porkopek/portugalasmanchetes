0fa210e4d00bf6d2973b011f1573288d28b57077
Porkopek <porkopek@users.noreply.github.com>
2020-09-03 21:32:21 +0100

    second

---
+++ build/precache-manifest.d7b62bf6b5a59ccbedb4746eb09709a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34df18162c2add914d3478b653e02241",
+    "url": "/portugalasmanchetes/index.html"
+  },
+  {
+    "revision": "780dfdac83ec6e7e3c95",
+    "url": "/portugalasmanchetes/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "f596b5ba594622bcb751",
+    "url": "/portugalasmanchetes/static/css/main.a8b31d64.chunk.css"
+  },
+  {
+    "revision": "780dfdac83ec6e7e3c95",
+    "url": "/portugalasmanchetes/static/js/2.1d836705.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portugalasmanchetes/static/js/2.1d836705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f596b5ba594622bcb751",
+    "url": "/portugalasmanchetes/static/js/main.4406adec.chunk.js"
+  },
+  {
+    "revision": "e3134afca5ed85465d90",
+    "url": "/portugalasmanchetes/static/js/runtime-main.851f3c0f.js"
+  },
+  {
+    "revision": "36938bbe277961386e7e309dcb30cbd8",
+    "url": "/portugalasmanchetes/static/media/es.36938bbe.svg"
+  },
+  {
+    "revision": "11093ee16c26a61acb6c3c1637e50ec7",
+    "url": "/portugalasmanchetes/static/media/global.11093ee1.svg"
+  },
+  {
+    "revision": "70a47eede02a7f19435d602e2294eb8b",
+    "url": "/portugalasmanchetes/static/media/pt.70a47eed.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/portugalasmanchetes/static/media/uk.e5564902.svg"
+  }
+]);
